33e374b17cacc979bf3f51f72df1289b539b68e9
lureilly1 <lureilly1@gmail.com>
2021-01-26 17:27:03 +0000

    initial commit

--- src/root.jsx
+++ src/root.jsx
@@ -1,7 +1,5 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import Table from './components/Table';
-import properties from './object-properties';
 
 export function render(element) {
   ReactDOM.render(<div>Hello JSX</div>, element);
